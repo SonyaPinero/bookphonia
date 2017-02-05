1b7f4bc282ce78ac26882cda061f392bcef48d3b
Sonya Pinero <sonyapinero@gmail.com>
2017-02-04 23:05:23 -0500

    separate index

--- src/server.js
+++ src/server.js
@@ -9,25 +9,4 @@ const server = express();
 
 server.use(express.static('./build/public'));
 
-server.get('*', (req, res) => {
-  match({location: req.url }, (err, redirect, props) => {
-      res.send(renderPage())
-  })
-})
-
-function renderPage() {
-  return `<!DOCTYPE html public="storage">
-    <html>
-        <head>
-            <meta charset=utf-8/>
-            <title>Test</title>
-        </head>
-        <body>
-            <div id=app>This is a test</div>
-            <script src="/app.js"></script>
-        </body>
-    </html>
-   `
-}
-
 server.listen(8080, () => console.log('Listening on port 8080'));
