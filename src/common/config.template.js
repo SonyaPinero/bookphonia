3d5ab23113bcab7c4d0fb2c73073ae6a1bebf125
Sonya Pinero <sonyapinero@gmail.com>
2017-06-25 12:42:27 -0400

    adding

---
+++ src/common/config.template.js
@@ -0,0 +1,13 @@
+export default {
+  aws: {
+    zone: '',
+    accessKeyId: '',
+    secretAccessKey: ''
+  },
+  spotify: {
+    clientId: '',
+    clientSecret: '',
+    token: '',
+    baseURL: 'https://api.spotify.com'
+  }
+};
