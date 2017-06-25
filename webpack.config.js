46984bf6f2107f5d03618528701718194775d33f
Sonya Pinero <sonyapinero@gmail.com>
2017-06-25 12:43:31 -0400

    adding again

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-
-  },
+  entry: './src/index.js',
   output: {
-
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
 
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
